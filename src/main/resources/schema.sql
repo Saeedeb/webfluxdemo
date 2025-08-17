bc039d6be71ba68fc627b21f5048ec7651697404
ebrahimi <ali.baran1987@gmail.com>
2025-08-17 16:39:37 +0330

    added role and user role to database

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,6 +5,8 @@
 
  ALTER TABLE users
      ADD COLUMN IF NOT EXISTS email varchar(255);
+ALTER TABLE users
+     ADD COLUMN IF NOT EXISTS username varchar(50);
 
 ALTER TABLE users
      ADD COLUMN IF NOT EXISTS password varchar(255);
@@ -17,3 +19,22 @@ ALTER TABLE users
      (email COLLATE pg_catalog."default" ASC NULLS LAST)
  WITH (deduplicate_items=True)
      TABLESPACE pg_default;
+
+ CREATE UNIQUE INDEX IF NOT EXISTS users_username_idx
+     ON public.users USING btree
+     (username COLLATE pg_catalog."default" ASC NULLS LAST)
+ WITH (deduplicate_items=True)
+     TABLESPACE pg_default;
+
+ CREATE TABLE  IF NOT EXISTS  roles (
+                          id int GENERATED ALWAYS AS IDENTITY  PRIMARY KEY,
+                          name VARCHAR(50) UNIQUE
+ );
+
+ CREATE TABLE  IF NOT EXISTS user_roles (
+                             user_id int,
+                             role_id int,
+                             PRIMARY KEY (user_id, role_id),
+                             FOREIGN KEY (user_id) REFERENCES users(id),
+                             FOREIGN KEY (role_id) REFERENCES roles(id)
+ );
